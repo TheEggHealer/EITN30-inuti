5ff346114d053d24e4ea8f53571fda23022a2474
IsakJacobsson <isak2jacobsson@gmail.com>
2023-02-16 14:42:31 +0000

    fixed indenting in rx_model

--- rx_model.py
+++ rx_model.py
@@ -4,21 +4,20 @@ from tuntap import TunTap
 
 def rx_thread(rx: RF24, interface: TunTap):
 
-    segments = [b''] * 256
+	segments = [b''] * 256
 
-    while True:
-        
-        if rx.available():
-            buffer = rx.read()
-            
-            if (buffer == bytes(0b1001)): 
-                # Entire ip package received
-                packet = b''.join(segments)
-                interface.write(packet)
-                packet = [b''] * 256
-            else:
-                segment_idx = struct.unpack('>B', buffer[0])
-                segment = buffer[1:]
-                segments[segment_idx] = segment
-
+	while True:
+			
+		if rx.available():
+			buffer = rx.read()
+			
+			if (buffer == bytes(0b1001)): 
+				# Entire ip package received
+				packet = b''.join(segments)
+				interface.write(packet)
+				packet = [b''] * 256
+			else:
+				segment_idx = struct.unpack('>B', buffer[0])
+				segment = buffer[1:]
+				segments[segment_idx] = segment
             
